f6833fa034b56cf49bc1d3044ee49248eaba6be2
= <=>
2022-05-23 11:27:19 +0700

    update

--- models/category.js
+++ models/category.js
@@ -2,13 +2,16 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // code here
+      category.belongsToMany(models.product, {
+        as: "products",
+        through: {
+          model: 'productCategory',
+          as: "bridge",
+        },
+        foreignKey: "idCategory"
+      })
     }
   }
   category.init(
--- models/product.js
+++ models/product.js
@@ -2,11 +2,6 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class product extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
       product.belongsTo(models.user, {
@@ -24,6 +19,14 @@ module.exports = (sequelize, DataTypes) => {
       });
 
       // code here
+      product.belongsToMany(models.category, {
+        as: "categories",
+        through: {
+          model: 'productCategory',
+          as: "bridge",
+        },
+        foreignKey: "idProduct"
+      })
     }
   }
   product.init(
