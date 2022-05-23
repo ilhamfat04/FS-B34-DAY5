d4bd5bf544aee3a9766c4127e9e917062b84034e
= <=>
2022-05-23 09:36:27 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
 const { user } = require('../../models')
 
-exports.addUsers = async (req, res) => {
+exports.addUser = async (req, res) => {
     try {
 
         await user.create(req.body)
@@ -20,8 +20,58 @@ exports.addUsers = async (req, res) => {
 
 exports.getUsers = async (req, res) => {
     // code here
+    try {
+        const users = await user.findAll({
+            attributes: {
+                exclude: ['password', 'createdAt', 'updatedAt']
+            }
+        })
+
+        // `SELECT name,email, status, id FROM`
+        res.send({
+            status: "success",
+            data: {
+                users
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: 'failed',
+            message: 'Server Error'
+        })
+    }
+
 }
 
 exports.getUser = async (req, res) => {
     // code here
+    try {
+
+        // return console.log(req.params);
+        const { id } = req.params
+        // const id = req.params.id
+
+        const data = await user.findOne({
+            where: {
+                status: "user"
+            },
+            attributes: {
+                exclude: ['password', 'createdAt', 'updatedAt']
+            }
+        })
+
+        res.send({
+            status: "success",
+            data: {
+                data
+            }
+        })
+
+    } catch (error) {
+        res.send({
+            status: 'failed',
+            message: 'Server Error'
+        })
+    }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 
 // Controller
 // import controller function here
-const { addUsers } = require('../controllers/user')
+const { addUser, getUsers, getUser } = require('../controllers/user')
 
 // Route
-router.post('/user', addUsers)
+router.post('/user', addUser)
 // add route here
+router.get('/users', getUsers)
+router.get('/user/:id', getUser)
 
 module.exports = router
