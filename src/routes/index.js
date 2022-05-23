809718da0e1b0789052b16da2685f13d30c5d4cf
= <=>
2022-05-23 09:49:21 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const { route } = require('express/lib/application')
 
 const router = express.Router()
 
 // Controller
 // import controller function here 
-const { addUsers, getUsers, getUser, updateUser } = require('../controllers/user')
+const { addUsers, getUsers, getUser, updateUser, deleteUser } = require('../controllers/user')
 
 // Route
 router.post('/user', addUsers)
@@ -12,5 +13,6 @@ router.get('/users', getUsers)
 router.get('/user/:id', getUser)
 router.patch('/user/:id', updateUser)
 // add route here
+router.delete('/user/:id', deleteUser)
 
 module.exports = router
