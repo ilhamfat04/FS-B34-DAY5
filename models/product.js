365e28a431f4142990814c1276742a255e30b2dc
= <=>
2022-05-23 11:01:43 +0700

    update

--- models/product.js
+++ models/product.js
@@ -4,14 +4,9 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class product extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
-      product.belongsTo(models.user,{
+      product.belongsTo(models.user, {
         as: 'user',
         foreignKey: {
           name: 'idUser'
