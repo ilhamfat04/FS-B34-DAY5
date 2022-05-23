8b3d80309180c5e48837de56aea58611997f8d88
= <=>
2022-05-23 09:16:25 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,10 @@ const router = express.Router()
 
 // Controller
 // import controller here
+const { addUser } = require('../controllers/user')
 
 // Route
 // add route here
-
+router.post('/user', addUser)
 
 module.exports = router
